3471157a9c765a640978bdb05985c3ab80e4f66b
idealist1508 <id1508-publicgithub@yahoo.com>
2015-02-16 15:13:07 +0100

    - Blob Properties

---
+++ Sources/Tomin.TotalCmd.AzureBlob/Forms/BlobPropertiesDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Tomin.TotalCmd.AzureBlob.Forms
+{
+    public partial class BlobPropertiesDialog : Form
+    {
+
+        private BlobPropertiesDialog()
+        {
+            InitializeComponent();
+        }
+
+        public static void Edit(ICloudBlob cloudBlob)
+        {
+            using (var dlg = new BlobPropertiesDialog())
+            {
+                dlg.contentTypeTextBox.Text = cloudBlob.Properties.ContentType;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    cloudBlob.Properties.ContentType = dlg.contentTypeTextBox.Text;
+                    cloudBlob.SetProperties();
+                }
+            }
+        }
+    }
+}
